cdf50624f2ce066403dfbec5d1bd8ff72ffcb913
eryczou <eryczou@gmail.com>
2017-10-03 22:04:41 -0400

    update more service layer files

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const Koa = require('koa');
 const Controller = require('koa-router');
 const mount = require('koa-mount');
+const querystring = require('querystring');
 const app = new Koa();
 const controller = new Controller();
 
@@ -32,14 +33,35 @@ controller.get('/ajax/index', ctx => {
     ctx.body = service.get_index_data();
 })
 
-controller.get('/ajax/search', ctx => {
+controller.get('/ajax/book', ctx => {
     ctx.set('Cache-Control', 'no-cache');
-    var queryString = require('querystring');
-    var params = queryString.parse(ctx.req._parsedUrl.query) ;
-    var start = params.start;
-    var end = params.end;
-    var keyword = params.keyword;
-    ctx.body = service.get_search_data(start, end, keyword);
+    let params = querystring.parse(ctx.req._parsedUrl.query) ;
+    let id = params.id;
+    if (!id)  id='';
+    ctx.body = service.get_book_data(id);
+})
+
+controller.get('/ajax/bookbacket', ctx=> {
+    ctx.set('Cache-Control', 'no-cache');
+    ctx.body = service.get_bookbacket_data();
+})
+
+controller.get('/ajax/channel', ctx => {
+    ctx.set('Cache-Control', 'no-cache');
+    let params = querystring.parse(ctx.req._parsedUrl.query) ;
+    let gender = params.gender;
+    if (!gender)  gender='';
+    ctx.body = service.get_channel_data(gender);
+})
+
+controller.get('/ajax/search', async ctx => {
+    ctx.set('Cache-Control', 'no-cache');
+    let queryString = require('querystring');
+    let params = queryString.parse(ctx.req._parsedUrl.query) ;
+    let start = params.start;
+    let end = params.end;
+    let keyword = params.keyword;
+    ctx.body = await service.get_search_data(start, end, keyword);
 })
 
 controller.all('*', ctx => {
--- service/webAppService.js
+++ service/webAppService.js
@@ -1,46 +1,49 @@
 const fs = require('fs');
+const axios = require('axios');
 
 exports.get_test_data = function () {
-    var content = fs.readFileSync('./mock/test.json', 'utf-8');
+    let content = fs.readFileSync('./mock/test.json', 'utf-8');
     return content;
 }
 
 exports.get_index_data = function() {
-    var content = fs.readFileSync('./mock/home.json', 'utf-8');
+    let content = fs.readFileSync('./mock/home.json', 'utf-8');
+    return content;
+}
+
+exports.get_book_data = function(id) {
+    if (!id)  id = '18218';
+    let content = fs.readFileSync('./mock/book/' + id + '.json', 'utf-8');
+    return content;
+}
+
+exports.get_bookbacket_data = function() {
+    let content = fs.readFileSync('./mock/bookbacket.json', 'utf-8');
+    return content;
+}
+
+exports.get_channel_data = function(gender) {
+    let content = fs.readFileSync('./mock/channel/' + gender + '.json', 'utf-8');
+    return content;
+}
+
+exports.get_rank_data = function() {
+    let content = fs.readFileSync('./mock/rank.json', 'utf-8');
     return content;
 }
 
 exports.get_search_data = function(start, end, keyword) {
-    return function(cb) {
-        var http = require('http');
-        var qs = require('querystring');
-
-        var data = {
-            s: keyword,
-            start: start,
-            end: end
-        };
-        var content = qs.stringify(data);
-
-        var http_request = {
-            hostname: 'dushu.xiaomi.com',
-            port: 80,
-            path: '/store/v0/lib/queryonebox?' + content
-        }
-
-        var req_obj = http.request(http_request, function(_res) {
-            var content = '';
-            _res.setEncoding('utf8');
-            _res.on('data', function(chunk) {
-                content += chunk;
-            });
-            _res.on('end', function () {
-                cb(null, content);
-            })
-        });
-
-        req_obj.on('error', function() {
-
+    return axios.get('http://dushu.xiaomi.com:80/store/v0/lib/query/onebox?', {
+            params: {
+                s: keyword,
+                start: start,
+                end: end
+            }
         })
-    }
+        .then(function(response) {
+            return JSON.stringify(response.data);
+        })
+        .catch(function(error) {
+            console.log(error);
+        });
 }
